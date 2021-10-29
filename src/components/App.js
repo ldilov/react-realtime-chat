08c0fbca079fcbd73bc6aae76de4041e735f44b5
Lazar Dilov <ldilov@isimarkets.com>
2021-10-30 00:58:40 +0300

    Styling

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,14 @@ import {Route, BrowserRouter as Router} from "react-router-dom";
 import HomeScreen from "./Screens/HomeScreen";
 import LoginScreen from "./Screens/LoginScreen";
 import AuthRoute from "./Routes/AuthRoute";
+import SignupScreen from "./Screens/SingupScreen";
 
 const App = () => {
     return (
         <Router>
             <AuthRoute exact path="/" component={HomeScreen} />
             <Route exact path="/login" component={LoginScreen} />
+            <Route exact path="/register" component={SignupScreen} />
         </Router>
     );
 }
